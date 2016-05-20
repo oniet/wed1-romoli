243ad6ea47e60ff01cd1bdb5d798c3838af18c7a
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-05-20 10:52:48 +0200

    Made almost everything jslint compilant. :expressionless:

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -1,159 +1,170 @@
-"use strict";
-/**
- * core
- */
-var calculator = new function(){
-    var register = "";
-    var result = 0; /* Result register, contains also inserted numbers */
-    var operator = "";
-    var isResult = false;
+(function () {
+    "use strict";
+    /**
+     * core
+     */
+    var calculator = function () {
+        var register = "";
+        var result = 0;
+        /* Result register, contains also inserted numbers */
+        var operator = "";
+        var isResult = false;
 
-    this.clear = function(){
-        operator = register = "";
-        result = 0;
-        isResult = false;
-    };
-
-    this.getRegister = function(){
-        return register;
-    };
-
-    this.getOperator = function(){
-        return operator;
-    };
-
-    this.getResult = function(){
-        return result;
-    };
-
-    this.performCalculation = function(){
-        isResult = true;
-        if(operator === "" || typeof(register) !== "number"){
-            register = "Invalid calculation";
-            return;
+        function setRegister() {
+            if (Number(register) > 0) {
+                return;
+            }
+            register = result;
+            result = 0;
+            isResult = false;
         }
-        switch(operator){
-            case "+":
-                result = register + result;
-                break;
-            case "-":
-                result = register - result;
-                break;
-            case "*":
-                result = register * result;
-                break;
-            case "/":
-                result = register / result;
-                break;
+
+        return {
+            clear: function () {
+                operator = "";
+                register = "";
+                result = 0;
+                isResult = false;
+            },
+
+            getRegister: function () {
+                return register;
+            },
+
+            getOperator: function () {
+                return operator;
+            },
+
+            getResult: function () {
+                return result;
+            },
+
+            performCalculation: function () {
+                isResult = true;
+                if (operator === "" || typeof(register) !== "number") {
+                    register = "Invalid calculation";
+                    return;
+                }
+                switch (operator) {
+                    case "+":
+                        result = register + result;
+                        break;
+                    case "-":
+                        result = register - result;
+                        break;
+                    case "*":
+                        result = register * result;
+                        break;
+                    case "/":
+                        result = register / result;
+                        break;
+                }
+                operator = "";
+                register = "";
+            },
+
+            enterNumber: function (num) {
+                if (isResult) {
+                    this.clear();
+                }
+                result = result * 10 + Number(num);
+            },
+
+
+            enterOperatorAdd: function () {
+                setRegister();
+                operator = "+";
+            },
+            enterOperatorSubtract: function () {
+                setRegister();
+                operator = "-";
+            },
+            enterOperatorMultiply: function () {
+                setRegister();
+                operator = "*";
+            },
+            enterOperatorDivide: function () {
+                setRegister();
+                operator = "/";
+            }
+        };
+    };
+
+
+    /**
+     * UI
+     */
+
+    var ui = function () {
+        var _calculator = new calculator();
+        var displayInput;
+        var displayOutput;
+
+        function updateDisplay() {
+            displayOutput.innerHTML = _calculator.getRegister() + " " + _calculator.getOperator();
+            displayInput.innerHTML = _calculator.getResult();
         }
-        operator = register = "";
+
+        return {
+
+            init: function (idInput, idOutput) {
+                displayInput = document.getElementById(idInput);
+                displayOutput = document.getElementById(idOutput);
+                displayOutput.innerHTML = "Welcome";
+            },
+
+            addOnClickForClass: function (className, func) {
+                var elements = document.getElementsByClassName(className);
+
+                var i;
+                for (i = 0; i < elements.length; i++) {
+                    elements[i].addEventListener('click', func, false);
+                }
+            },
+
+            handleCommandClick: function () {
+                switch (this.getAttribute("id")) {
+                    case "key-c":
+                        _calculator.clear();
+                        break;
+                    case "key-=":
+                        _calculator.performCalculation();
+                        break;
+                }
+                updateDisplay();
+            },
+
+            handleOperatorClick: function () {
+                switch (this.value) {
+                    case "+":
+                        _calculator.enterOperatorAdd();
+                        break;
+                    case "-":
+                        _calculator.enterOperatorSubtract();
+                        break;
+                    case "*":
+                        _calculator.enterOperatorMultiply();
+                        break;
+                    case "/":
+                        _calculator.enterOperatorDivide();
+                        break;
+                }
+                updateDisplay();
+            },
+
+            handleNumberClick: function () {
+                _calculator.enterNumber(this.value);
+                updateDisplay();
+            }
+        };
     };
 
-    this.enterNumber = function(num){
-        if(isResult){
-            this.clear();
-        }
-        result = result * 10 + Number(num);
-    };
+    window.addEventListener('load', function () {
+        var _ui = new ui();
+        _ui.init("input", "output");
 
-    function setRegister(){
-        if(Number(register) > 0){
-            return;
-        }
-        register = result;
-        result = 0;
-        isResult = false;
-    }
-
-    this.enterOperatorAdd = function(){
-        setRegister();
-        operator = "+";
-    };
-    this.enterOperatorSubtract = function(){
-        setRegister();
-        operator = "-";
-    };
-    this.enterOperatorMultiply = function(){
-        setRegister();
-        operator = "*";
-    };
-    this.enterOperatorDivide = function(){
-        setRegister();
-        operator = "/";
-    };
-};
-
-
-
-/**
- * UI
- */
-
-var ui = new function () {
-    var displayInput;
-    var displayOutput;
-
-    var self = this;
-
-    this.init = function(idInput, idOutput){
-        displayInput = document.getElementById(idInput);
-        displayOutput = document.getElementById(idOutput);
-        displayOutput.innerHTML = "Welcome";
-    };
-
-    this.addOnClickForClass = function(className, func){
-        var elements = document.getElementsByClassName(className);
-
-        for(var i = 0; i < elements.length; i++){
-            elements[i].addEventListener('click', func, false);
-        }
-    };
-
-    this.updateDisplay = function(){
-        displayOutput.innerHTML = calculator.getRegister() + " " + calculator.getOperator();
-        displayInput.innerHTML = calculator.getResult();
-    };
-
-    this.handleCommandClick = function(){
-        switch (this.getAttribute("id")) {
-            case "key-c":
-                calculator.clear();
-                break;
-            case "key-=":
-                calculator.performCalculation();
-                break;
-        }
-        self.updateDisplay();
-    };
-
-    this.handleOperatorClick = function(){
-        switch(this.value){
-            case "+":
-                calculator.enterOperatorAdd();
-                break;
-            case "-":
-                calculator.enterOperatorSubtract();
-                break;
-            case "*":
-                calculator.enterOperatorMultiply();
-                break;
-            case "/":
-                calculator.enterOperatorDivide();
-                break;
-        }
-        self.updateDisplay();
-    };
-
-    this.handleNumberClick = function(){
-        calculator.enterNumber(this.value);
-        self.updateDisplay();
-    };
-};
-
-window.addEventListener('load', function() {
-    ui.init("input","output");
-
-    ui.addOnClickForClass("number", ui.handleNumberClick);
-    ui.addOnClickForClass("operator", ui.handleOperatorClick);
-    ui.addOnClickForClass("command", ui.handleCommandClick);
-});
+        _ui.addOnClickForClass("number", _ui.handleNumberClick);
+        _ui.addOnClickForClass("operator", _ui.handleOperatorClick);
+        _ui.addOnClickForClass("command", _ui.handleCommandClick);
+    });
+}());
