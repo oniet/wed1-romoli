8c0f4cbb586e2d9587bbc2cb68de82b230460411
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-05-24 16:33:28 +0200

    Added font slider. Fixed dt format.

--- scripts/font-size.js
+++ scripts/font-size.js
@@ -1,1 +1,11 @@
+window.addEventListener('load', function () {
+    var slider = document.getElementById("font-slider");
+    var output = document.getElementById("font-size");
 
+    slider.addEventListener('change', function(){
+        var size = slider.value;
+        slider.title = size;
+        output.innerText = size;
+        document.body.style.fontSize = size+"px";
+    }, false);
+});
